a1211c4c1090fab3d92de9727419427cee291db0
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 20:53:52 +0100

    Modification de l'exercice avec rajout de if et else

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 function myFunction() {
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
-var result = Math.floor(firstNumber) * secondNumber;
-alert(result);
+if(isNaN(firstNumber)==false && isNaN(secondNumber)==false){
+  var result = Math.floor(firstNumber) * secondNumber;
+  alert(result);
+} else {
+  alert ('merci de remplir des chiffres');
+}
 }
