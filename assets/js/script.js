b9f5d18f233d525d8ef12f16d9553e4354f6d31a
Amadewe <dewevre.amandine@gmail.com>
2019-01-21 15:05:56 +0100

    Ajout et fin de l'exercice 5

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+var firstNumber = document.getElementById("firstNumber").value;
+var secondNumber = document.getElementById("secondNumber").value;
+var result = Math.floor(firstNumber) * secondNumber;
+alert(result);
+}
