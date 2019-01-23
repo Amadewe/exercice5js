2a9af60c5ce6b38a488fe6933c5a79b2be0230fe
Amadewe <dewevre.amandine@gmail.com>
2019-01-23 15:16:05 +0100

    modification de l'exercice 5 avec regex

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
 function myFunction() {
+var regex = new RegExp(/^[\d]+.+[\d]$/);
 var firstNumber = document.getElementById("firstNumber").value;
 var secondNumber = document.getElementById("secondNumber").value;
-if(isNaN(firstNumber)==false && isNaN(secondNumber)==false){
+if(regex.test(firstNumber) & regex.test(secondNumber)){
   var result = Math.trunc(firstNumber) * secondNumber;
   alert(result);
 } else {
-  alert ('merci de remplir des chiffres');
+  alert ('merci de remplir correctement tous les champs');
 }
 }
